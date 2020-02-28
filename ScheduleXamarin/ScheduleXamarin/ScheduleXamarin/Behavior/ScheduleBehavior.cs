f0d3ccc20e01487cde5ce2818b0aaebde0642f5a
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-02-28 15:41:29 +0530

    XAMARIN-33663 - sample added.

---
+++ ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/Behavior/ScheduleBehavior.cs
@@ -0,0 +1,34 @@
+﻿using Syncfusion.SfSchedule.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ScheduleXamarin
+{
+    public class ScheduleBehavior : Behavior<ContentPage>
+    {
+        SfSchedule schedule;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            schedule = bindable.FindByName<SfSchedule>("schedule");
+            schedule.AppointmentDrop += ScheduleOnAppointmentDrop;
+        }
+
+        private void ScheduleOnAppointmentDrop(object sender, AppointmentDropEventArgs e)
+        {
+            var dropTime = e.DropTime;
+            e.DropTime = new DateTime(dropTime.Year, dropTime.Month, dropTime.Day, dropTime.Hour, 0, 0);
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            schedule.AppointmentDrop -= ScheduleOnAppointmentDrop;
+            schedule = null;
+        }
+    }
+}
---
+++ ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/Model/Meeting.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ScheduleXamarin
+{
+    public class Meeting : INotifyPropertyChanged
+    {
+        private string eventname;
+        private DateTime from;
+        private DateTime to;
+        private bool isAllDay;
+        private Color color;
+
+        public string EventName
+        {
+            get { return eventname; }
+            set
+            {
+                eventname = value;
+                this.RaisePropertyChanged("EventName");
+            }
+        }
+
+        public DateTime From
+        {
+            get { return from; }
+            set
+            {
+                from = value;
+                this.RaisePropertyChanged("From");
+            }
+        }
+
+        public DateTime To
+        {
+            get { return to; }
+            set
+            {
+                to = value;
+                this.RaisePropertyChanged("To");
+            }
+        }
+
+        public bool IsAllDay
+        {
+            get { return isAllDay; }
+            set
+            {
+                isAllDay = value;
+                this.RaisePropertyChanged("IsAllDay");
+            }
+        }
+
+        public Color Color
+        {
+            get { return color; }
+            set
+            {
+                color = value;
+                this.RaisePropertyChanged("Color");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(String property)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
